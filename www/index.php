edc994b74f8a00f439bba7869987e1130d97ef31
katietsheehan <38252070+katietsheehan@users.noreply.github.com>
2019-09-25 16:30:37 -0400

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,2 +1,84 @@
-This is where I put anything I want. Hello World :).
-^C
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Katie Sheehan
+    CS321 Assignment 2 -->
+    <title>KTS Records Home</title>
+    <meta charset="utf-8">
+
+    <link href="style.css"
+type="text/css"
+rel="stylesheet">
+
+<div class = "topnav">
+    <nav>
+      <a class = "active" href="CS321Landing.html"> Home </a>
+      <a href="Studio.html">Studio </a>
+        <!-- make this a drop down
+         <ul>
+          <li>Booking</li>
+          <li>Equipment</li>
+        </ul> -->
+        <a href="Artists.html"> Artists</a>
+      <!--  <ul>
+          <li>All</li>
+          <li>Genre</li>
+        </ul>
+      </ul>-->
+    </nav>
+</div>
+
+  </head>
+
+  <body>
+
+<h1>
+ K.T.S Records
+</h1>
+
+<img src="https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-vinyl-player-icon-png-image_696317.jpg"
+class = "mainimage">
+
+<h2>
+  Home
+</h2>
+
+    <p>Welcome to KTS Records! We are a record company and studio designed for artists to succeed. We want to help you make the
+    music that you want to make. </p>
+
+    <p>More content will be added I havent finished that yet</p>
+
+    <p>Lorem ipsum dolor sit amet, usu movet admodum dissentias ne, sed id habeo illud.
+      Eum te numquam veritus habemus. Ad mutat vidisse similique per, per ut velit consul persequeris.
+      In duis qualisque qui. Sit et ignota partiendo, in alia assueverit reformidans mei.
+      No impetus definitionem ius, et sit tation facilis expetenda. Mel nihil legimus accusamus in,
+      affert virtute ex ius. Inani iudicabit ei per, justo melius oporteat sed eu. Quo vitae graeco efficiendi te.
+      Dolor partiendo dissentiunt ut cum, persecuti adversarium definitiones ex pro. Eu error
+      tibique partiendo pro, eam veniam legere deseruisse ut. Cum ne option disputationi. Sea choro
+      lobortis id, dicta audire in has, per integre dolorem et. Viris minimum eloquentiam cu duo.
+      Eu nam affert dictas dissentiunt, eum te sale posidonium, ad postulant tractatos petentium mel.
+      Ea nam illum paulo cotidieque, ex quo audire pericula aliquando. Mel eu adversarium concludaturque,
+      per ei idque iudico. Ei quaeque instructior sea. Has dictas nonumes ad, novum appetere ad mei.
+      Velit iusto ponderum ut sed, vix alienum molestiae no. In quot meliore mei, ut mel posse splendide.
+      Eum quas pericula neglegentur ut, his ut case luptatum suscipiantur, ei nemore propriae insolens pri.
+      In vim alii equidem ocurreret, ullum interesset voluptatibus usu te. Ea viderer omittam vix,
+      ex vim tantas eligendi, ut vis minim commune voluptatibus. Ut eos latine ponderum neglegentur,
+      et maiestatis suscipiantur est. Te graeci facilisis signiferumque eum, sit ea expetenda scripserit.
+      Facete fastidii an nam. Facilis accusam an mea, delicata repudiandae ea vix. In dicat dicit his,
+      error iusto cu has, quod virtute pertinacia no eam. Mazim nonumes verterem pro id. No quo facer
+      omnium scaevola. Ne iriure iuvaret facilis per, cum brute delicata eu. Quo mandamus intellegat
+      adversarium ad, aeque consul aperiri eos no. Mel ea insolens definiebas suscipiantur, ut scaevola
+      platonem ius. Id eam choro nostro, ut vim invidunt sapientem, et fuisset mentitum mediocrem nec.
+      Ex viris quidam vel, in tale assentior duo. Ipsum erant perpetua ad vis, at posse clita noluisse per.
+      Dico minimum no qui, primis perfecto mel eu, volumus repudiare ex has. Id quot reque expetendis eum.
+      Agam facer habemus et eum. At sit ignota populo. Cum in zril admodum, ad his causae expetendis. Quis deseruisse ei usu.
+      Ocurreret accusamus intellegat eu has, essent option commune ut has.</p>
+
+  </body>
+
+  <footer>
+    <code>Katie Sheehan -- CS 321</code>
+
+  </footer>
+
+</html>
