c896a7db59baebe3841ada65dd964684db7294ef
katietsheehan <38252070+katietsheehan@users.noreply.github.com>
2019-12-11 17:12:25 -0500

    Update and rename studio.php to contact.php

--- www/contact.php
+++ www/contact.php
@@ -3,15 +3,17 @@
   <head>
 
   <title>KTS Records | Studio</title>
-  <meta charset="utf-8">
+  <meta charset="utf-8" name="viewport" content="width=divice-width, initial-scale=1.0">
 
   <link href="css/style.css"
 type="text/css"
 rel="stylesheet">
 
-<script src="form.php"></script>
+<?php include "form.php" ?>
   <script src="javascript/script.js"></script>
-
+  </head>
+  <body>
+  <header>
 <h1> K.T.S Records </h1>
 <img src="https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-vinyl-player-icon-png-image_696317.jpg"
 class = "mainimage">
@@ -19,7 +21,7 @@ class = "mainimage">
 <div class = "topnav">
     <nav>
       <a href="index.php"> Home </a>
-      <a class = "active" href="studio.php">Studio </a>
+      <a class = "active" href="contact.php">Contact </a>
         <!-- make this a drop down
          <ul>
           <li>Booking</li>
@@ -34,43 +36,40 @@ class = "mainimage">
     </nav>
 </div>
 
-  </head>
+</header>
 
-  <body>
+  <h2>Contact</h2>
+  <p>Contact us to book the studio or with any questions!</p>
 
-  <h2>Studio</h2>
-  <p>This is our studio page. Here you can find what equipment we have and you can book time to use the studio</p>
 
-<h2>Booking</h2>
 
 <!-- FORM CODE FROM https://www.w3schools.com/html/tryit.asp?filename=tryhtml_form_mail
 MODIFIED USING CLASS NOTES AND EXAMPLES FROM 9/25/19 -->
 
-  <form class="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" enctype="text/plain">
+  <form class="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 
     <div class="formitem">
       <label for="name">Name: </label>
-      <div id="errorname" class="error"></div>
+      <div id="errorname" class="error"><?php echo $nameErr; ?></div>
       <input type="text" id="name" minlength="2" name="name" size=50>
     </div>
 
     <div class="formitem">
       <label for="email">Email: </label>
-      <div id="erroremail" class="error"></div>
+      <div id="erroremail" class="error"><?php echo $emailErr; ?></div>
       <input type="email" id="email" placeholder="name@example.com" name="email" size=50>
     </div>
 
     <div class="formitem">
       <label for="phone">Phone: </label>
-      <div id="errorphone" class="error"></div>
+      <div id="errorphone" class="error"><?php echo $phoneErr; ?></div>
       <input type="tel" id="phone" placeholder="000-000-0000" name="phone" size=50>
     </div>
 
     <div class="formitem">
       <label for="comment">Message: </label>
-      <div id="errorcomment" class="error"></div>
-      <textarea rows="5" cols="48" id = "comment"></textarea>
-      <!--<input type="text" id="comment" name="comment" size=50>-->
+      <div id="errorcomment" class="error"><?php echo $CommentErr; ?></div>
+      <input type="text" id="comment" name="comment" size=50>
     </div>
 
 
@@ -78,24 +77,12 @@ MODIFIED USING CLASS NOTES AND EXAMPLES FROM 9/25/19 -->
 <!--Comment:<br>
 <input type="text" name="comment" size="50"><br><br>-->
 
-<input type="button" value="Send" <!--onclick="checkAll()-->">
-<input type="reset" value="Reset">
+<input type="submit" name="submit" value="Send" onclick=<a href="thankyou.php">>
+<input type="reset" name="reset" value="Reset">
 
 
 </form>
-<?php
-echo "Your Input:";
-echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $phone;
-echo "<br>";
-echo $comment;
-?>
 
-</body>
-</html>
 
 
 
