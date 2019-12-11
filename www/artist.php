844544ea206e2739704f169f5ec9736c9a600d65
katietsheehan <38252070+katietsheehan@users.noreply.github.com>
2019-12-11 17:13:03 -0500

    Update artist.php

--- www/artist.php
+++ www/artist.php
@@ -1,7 +1,7 @@
 <?php
-include_once 'db.php';
-
+include 'db.php';
 ?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -12,6 +12,10 @@ include_once 'db.php';
   <link href="css/style.css"
 type="text/css"
 rel="stylesheet">
+  </head>
+  
+  <body>
+      <header>
 
 <h1> K.T.S Records </h1>
 <img src="https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-vinyl-player-icon-png-image_696317.jpg"
@@ -20,7 +24,7 @@ class = "mainimage">
 <div class = "topnav">
     <nav>
       <a href="index.php"> Home </a>
-      <a href="studio.php">Studio </a>
+      <a href="contact.php">Contact </a>
         <!-- make this a drop down
          <ul>
           <li>Booking</li>
@@ -34,27 +38,40 @@ class = "mainimage">
       </ul>-->
     </nav>
 </div>
-
-  </head>
-
-  <body>
-
+      </header>
 
   <h2>Artists</h2>
   <p>Here you will find a list of artists and a list of genres those artists belong to.</p>
-  <p>
+  
+ <div class = "table">
+  <table>
 <?php
-$sql="SELECT ArtistName FROM Artists;";
+$sql="SELECT * FROM Artists;";
 $result = mysqli_query($conn, $sql);
 $resultCheck = mysqli_num_rows($result);
+$array;
+$count = 0;
 if ($resultCheck >0){
+   
     while($row=mysqli_fetch_assoc($result)){
-        echo $row["ArtistName"];
-        echo "\n";
+        if ($count%3==0):
+            echo"<tr>";
+        endif;
+        echo "<th><img src=\"".$row["Photo"]."\" class = \"tableimage\">";
+        echo $row["ArtistName"]."<br>";
+        echo $row["Genre"]."</th>";
+        echo "<br>";  
+        $count = $count + 1;
+        if ($count%3==0):
+            echo "</tr>";
+        endif;
 }
+echo "</tr>";
 }
-?>
-  </p>
+?>   
+  </table>
+  </div>
+  
 
   </body>
 
@@ -64,5 +81,4 @@ if ($resultCheck >0){
   </footer>
 
   </html>
-
 
